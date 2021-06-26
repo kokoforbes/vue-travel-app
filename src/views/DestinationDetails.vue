83c596bd8a0d4084c10f9d2efa51e3fbfc819b4d
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-06-26 21:16:05 +0100

    Created experience section

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,14 +1,34 @@
 <template>
-  <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-      <img
-        :src="require(`@/assets/${destination.image}`)"
-        :alt="destination.name"
-      />
-      <p>{{ destination.description }}</p>
-    </div>
-  </section>
+  <div>
+    <section class="destination">
+      <h1>{{ destination.name }}</h1>
+      <div class="destination-details">
+        <img
+          :src="require(`@/assets/${destination.image}`)"
+          :alt="destination.name"
+        />
+        <p>{{ destination.description }}</p>
+      </div>
+    </section>
+
+    <section class="experiences">
+      <h2>Top experiences in {{ destination.name }}</h2>
+
+      <div class="cards">
+        <div
+          class="card"
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+        >
+          <img
+            :src="require(`@/assets/${experience.image}`)"
+            :alt="experience.name"
+          />
+          <span class="img__text">{{ experience.name }}</span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -51,5 +71,29 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+
+.cards {
+  display: flex;
+}
+
+.card img {
+  max-height: 200px;
+}
+
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
