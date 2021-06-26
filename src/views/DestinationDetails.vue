0cd252704cff2a346bb6e2dda14ff280fd1e01fd
onikoko abdullahi <Onikokoabdullahi@gmail.com>
2021-06-26 12:23:38 +0100

    added description to destinationDetails page

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>The destination is: {{ this.$route.params.id }}</div>
+  <section class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img
+        :src="require(`@/assets/${destination.image}`)"
+        :alt="destination.name"
+      />
+      <p>{{ destination.description }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
+import store from "@/store.js";
 export default {
   name: "DestinationDetials",
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+    };
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        (destination) => destination.id === this.destinationId
+      );
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+img {
+  max-width: 600px;
+  max-height: 400px;
+  width: 100%;
+  height: auto;
+}
+
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
